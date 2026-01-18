742f4826a1f4306b4042dc360b16c83f0a1fbac2
kash-arya <mraryan7421@gmail.com>
2026-01-18 16:48:46 +0530

    Add files via upload

---
+++ Assignment-1/Q1Binary Search.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int binarysearch(int arr[], int n, int key)
+{
+    int left = 0, right = n - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    int arr[] = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int target = 23;
+
+    int result = binarysearch(arr, n, target);
+
+    if (result != -1)
+        cout << "Element found at index " << result << endl;
+    else
+        cout << "Element not found" << endl;
+
+    return 0;
+}
